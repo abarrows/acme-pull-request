8fba6fe74e1347be63c0dcb1536e49de3d308f6e
Andrew Barrows <acbarrows@gmail.com>
2024-08-02 08:11:02 -0500

    Update userService.test.ts
    Reverting back since it's formatting and not pertinent.

--- src/api/user/__tests__/userService.test.ts
+++ src/api/user/__tests__/userService.test.ts
@@ -1,30 +1,30 @@
-import { StatusCodes } from 'http-status-codes';
-import type { Mock } from 'vitest';
+import { StatusCodes } from "http-status-codes";
+import type { Mock } from "vitest";
 
-import type { User } from '@/api/user/userModel';
-import { UserRepository } from '@/api/user/userRepository';
-import { UserService } from '@/api/user/userService';
+import type { User } from "@/api/user/userModel";
+import { UserRepository } from "@/api/user/userRepository";
+import { UserService } from "@/api/user/userService";
 
-vi.mock('@/api/user/userRepository');
+vi.mock("@/api/user/userRepository");
 
-describe('userService', () => {
+describe("userService", () => {
   let userServiceInstance: UserService;
   let userRepositoryInstance: UserRepository;
 
   const mockUsers: User[] = [
     {
-      accountType: 'freeUser',
       id: 1,
-      name: 'Alice',
-      lastLoginDate: new Date(),
+      name: "Alice",
+      email: "alice@example.com",
+      age: 42,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
     {
-      accountType: 'paidUser',
-      id: 1,
-      name: 'Bob',
-      lastLoginDate: new Date(),
+      id: 2,
+      name: "Bob",
+      email: "bob@example.com",
+      age: 21,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -35,8 +35,8 @@ describe('userService', () => {
     userServiceInstance = new UserService(userRepositoryInstance);
   });
 
-  describe('findAll', () => {
-    it('return all users', async () => {
+  describe("findAll", () => {
+    it("return all users", async () => {
       // Arrange
       (userRepositoryInstance.findAllAsync as Mock).mockReturnValue(mockUsers);
 
@@ -46,11 +46,11 @@ describe('userService', () => {
       // Assert
       expect(result.statusCode).toEqual(StatusCodes.OK);
       expect(result.success).toBeTruthy();
-      expect(result.message).equals('Users found');
+      expect(result.message).equals("Users found");
       expect(result.responseObject).toEqual(mockUsers);
     });
 
-    it('returns a not found error for no users found', async () => {
+    it("returns a not found error for no users found", async () => {
       // Arrange
       (userRepositoryInstance.findAllAsync as Mock).mockReturnValue(null);
 
@@ -60,15 +60,13 @@ describe('userService', () => {
       // Assert
       expect(result.statusCode).toEqual(StatusCodes.NOT_FOUND);
       expect(result.success).toBeFalsy();
-      expect(result.message).equals('No Users found');
+      expect(result.message).equals("No Users found");
       expect(result.responseObject).toBeNull();
     });
 
-    it('handles errors for findAllAsync', async () => {
+    it("handles errors for findAllAsync", async () => {
       // Arrange
-      (userRepositoryInstance.findAllAsync as Mock).mockRejectedValue(
-        new Error('Database error')
-      );
+      (userRepositoryInstance.findAllAsync as Mock).mockRejectedValue(new Error("Database error"));
 
       // Act
       const result = await userServiceInstance.findAll();
@@ -76,15 +74,13 @@ describe('userService', () => {
       // Assert
       expect(result.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
       expect(result.success).toBeFalsy();
-      expect(result.message).equals(
-        'An error occurred while retrieving users.'
-      );
+      expect(result.message).equals("An error occurred while retrieving users.");
       expect(result.responseObject).toBeNull();
     });
   });
 
-  describe('findById', () => {
-    it('returns a user for a valid ID', async () => {
+  describe("findById", () => {
+    it("returns a user for a valid ID", async () => {
       // Arrange
       const testId = 1;
       const mockUser = mockUsers.find((user) => user.id === testId);
@@ -96,16 +92,14 @@ describe('userService', () => {
       // Assert
       expect(result.statusCode).toEqual(StatusCodes.OK);
       expect(result.success).toBeTruthy();
-      expect(result.message).equals('User found');
+      expect(result.message).equals("User found");
       expect(result.responseObject).toEqual(mockUser);
     });
 
-    it('handles errors for findByIdAsync', async () => {
+    it("handles errors for findByIdAsync", async () => {
       // Arrange
       const testId = 1;
-      (userRepositoryInstance.findByIdAsync as Mock).mockRejectedValue(
-        new Error('Database error')
-      );
+      (userRepositoryInstance.findByIdAsync as Mock).mockRejectedValue(new Error("Database error"));
 
       // Act
       const result = await userServiceInstance.findById(testId);
@@ -113,11 +107,11 @@ describe('userService', () => {
       // Assert
       expect(result.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
       expect(result.success).toBeFalsy();
-      expect(result.message).equals('An error occurred while finding user.');
+      expect(result.message).equals("An error occurred while finding user.");
       expect(result.responseObject).toBeNull();
     });
 
-    it('returns a not found error for non-existent ID', async () => {
+    it("returns a not found error for non-existent ID", async () => {
       // Arrange
       const testId = 1;
       (userRepositoryInstance.findByIdAsync as Mock).mockReturnValue(null);
@@ -128,7 +122,7 @@ describe('userService', () => {
       // Assert
       expect(result.statusCode).toEqual(StatusCodes.NOT_FOUND);
       expect(result.success).toBeFalsy();
-      expect(result.message).equals('User not found');
+      expect(result.message).equals("User not found");
       expect(result.responseObject).toBeNull();
     });
   });
